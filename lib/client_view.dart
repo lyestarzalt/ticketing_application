ddb167543ea8331382b64e46d57e2f803bc0e9dc
Lyes Tarzalt <lyestarzalt@gmail.com>
2023-02-24 03:41:24 +0800

    improve client UI+ fix wrong last num

--- lib/client_view.dart
+++ lib/client_view.dart
@@ -18,6 +18,9 @@ class _TicketDashboardState extends State<TicketDashboard> {
     super.dispose();
   }
 
+// hackery way to force the child streambuilder to rebuild.
+
+  int _highestNumber = -1;
   Future _showTicketNumberPopup(
       BuildContext contextArg, String ticketNumber) async {
     showDialog(
@@ -68,8 +71,16 @@ class _TicketDashboardState extends State<TicketDashboard> {
               return const CircularProgressIndicator();
             } else if (ticketSnapshot.hasData) {
               // last ticket number
-              var lastTicket = ticketSnapshot.data!.docs.first.data() as Map;
-              var lastTicketNumber = lastTicket['ticket_number'];
+              var ticketDocs = ticketSnapshot.data!.docs;
+              int maxTicketNumber = 0;
+              ticketDocs.forEach((doc) {
+                var data = doc.data() as Map<String, dynamic>?;
+                int? ticketNumber = data != null ? data['ticket_number'] : null;
+
+                if (ticketNumber != null && ticketNumber > maxTicketNumber) {
+                  maxTicketNumber = ticketNumber;
+                }
+              });
 
               return StreamBuilder<QuerySnapshot>(
                 stream: FirebaseFirestore.instance
@@ -79,20 +90,25 @@ class _TicketDashboardState extends State<TicketDashboard> {
                     AsyncSnapshot<QuerySnapshot> counterSnapshot) {
                   if (counterSnapshot.hasError) {
                     return Text('Error: ${counterSnapshot.error}');
-                  } else if (!counterSnapshot.hasData) {
-                    return const CircularProgressIndicator();
-                  } else if (counterSnapshot.connectionState ==
-                      ConnectionState.waiting) {
+                  } else if (!counterSnapshot.hasData ||
+                      counterSnapshot.connectionState ==
+                          ConnectionState.waiting) {
                     return const CircularProgressIndicator();
                   } else if (counterSnapshot.hasData) {
+                    // Get the highest ticket number from the counters,
+                    // because the last ticket number is not necessarily
+                    // the highest number that is being served.
                     var counterDocs = counterSnapshot.data!.docs;
+
                     int highestNumber = -1;
+
                     for (var doc in counterDocs) {
                       int currentNumber = doc['current_ticket'];
                       if (currentNumber > highestNumber) {
                         highestNumber = currentNumber;
                       }
                     }
+
                     print('highest number: $highestNumber');
                     // counters -> counter1 -> currentNumber, lastNumber, status
                     // get all documents and assign to a variable each
@@ -104,65 +120,112 @@ class _TicketDashboardState extends State<TicketDashboard> {
                     });
 
                     return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         SizedBox(
                           height: MediaQuery.of(context).size.height * 0.5,
                           width: MediaQuery.of(context).size.width * 0.8,
                           child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Card(
+                                elevation: 4,
+                                margin: EdgeInsets.symmetric(horizontal: 16),
                                 child: Column(
+                                  mainAxisSize: MainAxisSize.min,
                                   children: [
+                                    SizedBox(height: 16),
                                     Text(
-                                        "Now Serving: $highestNumber"),
-                                    Text("Last Number: $lastTicketNumber"),
+                                      "Now Serving",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.blueGrey[800],
+                                      ),
+                                    ),
+                                    SizedBox(height: 16),
+                                    Text(
+                                      highestNumber.toString(),
+                                      style: TextStyle(
+                                        fontSize: 48,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.blueGrey[800],
+                                      ),
+                                    ),
+                                    SizedBox(height: 32),
+                                    Text(
+                                      "Last Number: $maxTicketNumber",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.blueGrey[800],
+                                      ),
+                                    ),
+                                    SizedBox(height: 32),
                                     ElevatedButton(
                                       onPressed: () => generateTicketNumber()
                                           .then((value) =>
                                               _showTicketNumberPopup(
                                                   context, value)),
-                                      child: Text("Take a Number"),
+                                      style: ElevatedButton.styleFrom(
+                                        padding: EdgeInsets.symmetric(
+                                          horizontal: 64,
+                                          vertical: 24,
+                                        ),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                        ),
+                                      ),
+                                      child: Text(
+                                        "Take a Number",
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
                                     ),
+                                    SizedBox(height: 16),
                                   ],
                                 ),
                               ),
                             ],
                           ),
                         ),
-                        Container(
-                          child: Card(
-                            child: Row(
-                              children: [
-                                CounterCard(
-                                  status: _counters['counter1']!['status'],
-                                  number: '1',
-                                  lastNumber:
-                                      _counters['counter1']!['last_ticket'],
-                                  isOnline: _counters['counter1']!['is_online'],
-                                ),
-                                CounterCard(
-                                  status: _counters['counter2']!['status'],
-                                  number: '2',
-                                  lastNumber:
-                                      _counters['counter2']!['last_ticket'],
-                                  isOnline: _counters['counter2']!['is_online'],
-                                ),
-                                CounterCard(
-                                  status: _counters['counter3']!['status'],
-                                  number: '3',
-                                  lastNumber:
-                                      _counters['counter3']!['last_ticket'],
-                                  isOnline: _counters['counter3']!['is_online'],
-                                ),
-                                CounterCard(
-                                  status: _counters['counter4']!['status'],
-                                  number: '4',
-                                  lastNumber:
-                                      _counters['counter4']!['last_ticket'],
-                                  isOnline: _counters['counter4']!['is_online'],
-                                ),
-                              ],
-                            ),
+                        Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              CounterCard(
+                                status: _counters['counter1']!['status'],
+                                number: '1',
+                                lastNumber:
+                                    _counters['counter1']!['last_ticket'],
+                                isOnline: _counters['counter1']!['is_online'],
+                              ),
+                              CounterCard(
+                                status: _counters['counter2']!['status'],
+                                number: '2',
+                                lastNumber:
+                                    _counters['counter2']!['last_ticket'],
+                                isOnline: _counters['counter2']!['is_online'],
+                              ),
+                              CounterCard(
+                                status: _counters['counter3']!['status'],
+                                number: '3',
+                                lastNumber:
+                                    _counters['counter3']!['last_ticket'],
+                                isOnline: _counters['counter3']!['is_online'],
+                              ),
+                              CounterCard(
+                                status: _counters['counter4']!['status'],
+                                number: '4',
+                                lastNumber:
+                                    _counters['counter4']!['last_ticket'],
+                                isOnline: _counters['counter4']!['is_online'],
+                              ),
+                            ],
                           ),
                         ),
                       ],
@@ -200,6 +263,7 @@ class CounterCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       color: isOnline ? null : Colors.grey[200],
+      elevation: 4,
       child: Stack(
         children: [
           SizedBox(
@@ -209,22 +273,36 @@ class CounterCard extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
-                    child: SizedBox(
-                  height: 30,
-                )), // Empty widget to create space at the top
-                Text("Counter $number"),
-                Text('Last Number: $lastNumber'),
+                  child: SizedBox(
+                    height: 30,
+                  ),
+                ), // Empty widget to create space at the top
+                Text(
+                  "Counter $number",
+                  style: TextStyle(
+                    fontSize: 20, // Increase font size
+                    fontWeight: FontWeight.bold, // Add bold style
+                  ),
+                ),
+                SizedBox(height: 10), // Add space between text elements
+                Text(
+                  'Last Number: $lastNumber',
+                  style: TextStyle(
+                    fontSize: 16, // Increase font size
+                  ),
+                ),
                 Expanded(
-                    child:
-                        SizedBox()), // Empty widget to create space at the bottom
+                  child: SizedBox(),
+                ),
               ],
             ),
           ),
           Positioned(
-            top: 0,
-            right: 0,
+            top: 5,
+            right: 5,
             child: Icon(
               Icons.circle,
+              size: 30,
               color:
                   isOnline ? (status ? Colors.green : Colors.red) : Colors.grey,
             ),
